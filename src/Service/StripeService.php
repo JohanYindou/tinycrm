6ab246dc7f0d8f192fe1b86fc98c09de06f088a2
Johan Yindou <jyindou@gmail.com>
2024-02-05 15:27:49 +0100

    Redirect to payment success/cancel pages instead of hard-coded URLs (generated by blackbox.ai )

--- src/Service/StripeService.php
+++ src/Service/StripeService.php
@@ -29,8 +29,8 @@ class StripeService
                 'quantity' => 1,
             ]],
             'mode' => 'payment',
-            'success_url' => $YOUR_DOMAIN . '/success.html',
-            'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+            'success_url' => $YOUR_DOMAIN . '/payment/success',
+            'cancel_url' => $YOUR_DOMAIN . '/payment/cancel',
         ]);
 
         header("HTTP/1.1 303 See Other");
