77ca549e4c8624c7153ca255a8b4851d2fa2ac4a
Johan Yindou <jyindou@gmail.com>
2024-02-06 12:31:22 +0100

    Refactor payment controller to use new mailer service and add transaction entity

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -2,26 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Client;
 use App\Form\PaymentType;
+use App\Entity\Transaction;
 use App\Service\StripeService;
+use Symfony\Component\Mime\Email;
 use App\Repository\OffreRepository;
 use App\Repository\ClientRepository;
-use App\Repository\TransactionRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/payment')]
 class PaymentController extends AbstractController
 {
     #[Route('/', name: 'app_payment')]
     public function index(
+        // Injection de dépendances
         Request $request,
         StripeService $stripeService,
         OffreRepository $offres,
         ClientRepository $clients,
-        TransactionRepository $transactions,
+        MailerInterface $mailer,
+        EntityManagerInterface $em,
     ): Response {
         // Création du formulaire
         $form = $this->createForm(PaymentType::class);
@@ -29,19 +35,47 @@ class PaymentController extends AbstractController
 
         // Traitement du formulaire si soumis et valide
         if ($form->isSubmitted() && $form->isValid()) {
-            $data = $form->getData();
-            $offre = $offres->findOneBy(['id' => $data['offre']->getId()]);
-            $clientEmail = $clients->findOneBy(['id' => $data['client']->getId()])->getEmail();
+            $data = $form->getData(); // Récupération des donnée
+            $offre = $offres->findOneBy(['id' => $data['offre']->getId()]); // Récupération de l'offre
+            $clientEmail = $clients->findOneBy(['id' => $data['client']->getId()])->getEmail(); // Récupération de l'email du client
+            $client = $clients->findOneBy(['id' => $data['client']->getId()]); // Récupération du client
+
 
             // Création du lien
-            $apiKey = $this->getParameter('STRIPE_API_KEY_SECRET');
+            $apiKey = $this->getParameter('STRIPE_API_KEY_SECRET'); //
             $link = $stripeService->makePayment(
                 $apiKey,
                 $offre->getMontant(),
                 $offre->getTitre(),
                 $clientEmail
             );
-            // Envoie du lien au client
+
+            // Envoi du mail
+            $email = (new Email())
+                ->from('jyindou@example.com')
+                ->to($clientEmail)
+                ->priority(Email::PRIORITY_HIGH)
+                ->subject('Merci de procéder au paiement de votre offre')
+                ->html('<div style="background-color: #f4f4f4; padding: 20px; text-align: center; font-family: Arial, sans-serif;">
+                        <h1>Bonjour '.$client->getNomComplet().'</h1><br><br>
+                        <p>Vous avez sélectioné l\'offre suivante : '.$offre->getTitre().'</p>
+                        <p>Le montant à régler est de '.$offre->getMontant().' €</p>
+                        <p>Voici le lien pour effectuer le reglement de votre offre :</p>
+                        <a href='.$link.' target="_blank">Payer</a><br>
+                        <hr>
+                        <p> Ce lien est valide pour une durée limitée.</p>
+                ');
+            $mailer->send($email);
+
+            // Enregistrement de la transaction
+            $transaction = new Transaction();
+            $transaction
+                ->setClient($data['client'])
+                ->setMontant($offre->getMontant())
+                ->setStatut('En attente')
+                ->setDate(new \DateTime());
+            $em->persist($transaction);
+            $em->flush();
         }
 
         // Affichage du formulaire ou page d'erreur
